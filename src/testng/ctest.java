63b4d24ec9ef72139dac04e11b05e5dff6e864da
pallijanaki <pallijanaki1@gmail.com>
2017-10-12 03:13:15 -0700

    Gmail and talentify login projects-mamatha

---
+++ src/testng/ctest.java
@@ -0,0 +1,26 @@
+package testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ctest {
+public static void main(String[] args) throws InterruptedException {
+
+	System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+
+   driver.get("https://accounts.google.com/signin");
+   driver.findElement(By.id("identifierId")).clear();
+   driver.findElement(By.id("identifierId")).sendKeys("mamathakanneboina448@gmail.com");
+   driver.findElement(By.cssSelector("content.CwaK9")).click();
+   Thread.sleep(2000);
+   driver.findElement(By.name("password")).sendKeys("mamatha552");
+   driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
+   driver.findElement(By.cssSelector("span.gb_7a.gbii")).click();
+   driver.findElement(By.id("gb_71")).click();
+   
+
+
+}
+}
