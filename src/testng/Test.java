a860b322038974de45409b3b67dd4e6fa4058334
pallijanaki <pallijanaki1@gmail.com>
2017-10-12 04:18:24 -0700

    GMAIL AND TALENTIFY LOGIN PROJECTS - MAMATHA

---
+++ src/testng/Test.java
@@ -0,0 +1,29 @@
+package testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+public static void main(String[] args) throws InterruptedException {
+
+	System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+
+	    driver.get("http://talentify.in/login.jsp");
+
+	  // driver.findElement(By.xpath("//div[@id='navbar-collapse-1']/ul/li[7]/a")).click();
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys("munnyammu448@gmail.com");
+	    driver.findElement(By.name("password")).clear();
+	    driver.findElement(By.name("password")).sendKeys("test123");
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    
+	    driver.findElement(By.xpath("//a[contains(text(),'Welcome\n						   NOT_PROVIDED')]")).click();
+	    driver.findElement(By.linkText("Logout")).click();
+	    // ERROR: Caught exception [unknown command []]
+	  }
+	    
+	   
+	  }
+	
--- src/testng/ntest.java
+++ src/testng/ntest.java
@@ -1,75 +1,27 @@
 package testng ;
-	
-	import java.util.concurrent.TimeUnit;
-	import org.junit.*;
-	import static org.junit.Assert.*;
-	import org.openqa.selenium.*;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	public class ntest {
-	  private WebDriver driver;
-	  private boolean acceptNextAlert = true;
-	  private StringBuffer verificationErrors = new StringBuffer();
 
-	  @Before
-	  public void setUp() throws Exception {
-	    driver = new FirefoxDriver();
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	  
-	  public void testMm() throws Exception {
-	    driver.get("https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
-	    driver.findElement(By.id("identifierId")).clear();
-	    driver.findElement(By.id("identifierId")).sendKeys("mamathakanneboina448@gmail.com");
-	    driver.findElement(By.cssSelector("content.CwaK9")).click();
-	    driver.findElement(By.name("password")).clear();
-	    driver.findElement(By.name("password")).sendKeys("mamatha552");
-	    driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
-	    driver.findElement(By.cssSelector("span.gb_7a.gbii")).click();
-	    driver.findElement(By.id("gb_71")).click();
-	  }
+public class ntest {
+public static void main(String[] args) throws InterruptedException {
 
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
+	System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
 
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
+   driver.get("https://accounts.google.com/signin");
+   driver.findElement(By.id("identifierId")).clear();
+   driver.findElement(By.id("identifierId")).sendKeys("mamathakanneboina448@gmail.com");
+   driver.findElement(By.cssSelector("content.CwaK9")).click();
+   Thread.sleep(2000);
+   driver.findElement(By.name("password")).sendKeys("mamatha552");
+   driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
+   driver.findElement(By.cssSelector("span.gb_7a.gbii")).click();
+   driver.findElement(By.id("gb_71")).click();
+   
 
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
 
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-	}
-
+}
+}
 
